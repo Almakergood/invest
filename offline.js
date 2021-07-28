1615eb6aee2e6f9487767c2d5ac9a9f91781a575
Almakergood <49840366+Almakergood@users.noreply.github.com>
2021-07-28 19:31:35 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1627482546,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"pro-sheet0.png",
+		"add-sheet0.png",
+		"proinfo-sheet0.png",
+		"proinfo-sheet1.png",
+		"sprite2-sheet0.png",
+		"bg1-sheet0.png",
+		"bg3-sheet0.png",
+		"bg2-sheet0.png",
+		"ico1-sheet0.png",
+		"back-sheet0.png",
+		"sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"SweetAlert.css",
+		"SweetAlert.js"
+	]
+}
